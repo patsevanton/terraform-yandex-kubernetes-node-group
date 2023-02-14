dafe0aaa82a80a92d5e2c9cc64be40b6a9a148bd
Anton Patsev <patsev.anton@gmail.com>
2023-02-14 10:46:01 +0600

    Remove yandex_iam_service_account sa-k8s-node-group

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-resource "yandex_iam_service_account" "sa-k8s-node-group" {
-  folder_id = var.folder_id
-  name      = "sa-k8s-node-group"
-}
-
-resource "yandex_resourcemanager_folder_iam_member" "sa-k8s-node-group-permissions" {
-  folder_id = var.folder_id
-  role      = "container-registry.images.puller"
-  member    = "serviceAccount:${yandex_iam_service_account.sa-k8s-node-group.id}"
-}
-
 resource "yandex_kubernetes_node_group" "k8s_node_group" {
   cluster_id  = var.cluster_id
   name        = var.node_group_name
